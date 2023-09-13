cd518279258bce032557631bb6bdec226b3a6cc7
naresh <nareshnaik209080@gmail.com>
2023-09-13 11:43:50 +0530

    css-3-Flex-Template-1

---
+++ assets/css/box.js
@@ -0,0 +1,19 @@
+const boxContainer = document.querySelector('#boxes');
+const btn = document.getElementById('btn');
+
+//event call
+btn.addEventListener('click', function(){
+    boxContainer.classList.toggle('big');
+});
+
+//custom function to craete boxes
+function createBoxes() {
+    for(let i=0; i<4; i++){ //column
+        for(let j=0; j<4; j++){ //row
+            const box = document.createElement('div');
+            box.classList.add('box')
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+            boxContainer.appendChild(box);
+        }
+    }
+}
